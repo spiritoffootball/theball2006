b7da2552f38c1dc1c697881fee6d7761d2009d5c
Christian Wach <needle@haystack.co.uk>
2018-04-30 13:13:43 +0100

    codestyle

--- assets/includes/site_banner.php
+++ assets/includes/site_banner.php
@@ -1,37 +1,42 @@
-<!-- site_banner.php -->
+<?php /*
+================================================================================
+Site Banner Template
+================================================================================
+AUTHOR: Christian Wach <needle@haystack.co.uk>
+--------------------------------------------------------------------------------
+NOTES
+
+--------------------------------------------------------------------------------
+*/
+
+?><!-- assets/includes/site_banner.php -->
 
 <div id="site_banner" class="clearfix">
-<div id="site_banner_inner">
 
+	<div id="site_banner_inner">
 
+		<?php include( get_template_directory().'/assets/includes/network.php' ); ?>
 
-<?php include( get_template_directory().'/assets/includes/network.php' ); ?>
+		<div id="splash">
 
+			<a href="/wp-content/blogs.dir/7/files/2009/11/the_ball_hands_blue.jpg"><img src="/wp-content/blogs.dir/7/files/2009/11/the_ball_hands_blue-200x150.jpg" alt="The logo held aloft" title="The logo held aloft" width="200" height="150" class="alignnone size-thumbnail-200 wp-image-264" /></a>
 
+		</div><!-- /splash -->
 
-<div id="splash">
+		<div id="banner_copy">
 
-<a href="/wp-content/blogs.dir/7/files/2009/11/the_ball_hands_blue.jpg"><img src="/wp-content/blogs.dir/7/files/2009/11/the_ball_hands_blue-200x150.jpg" alt="The logo held aloft" title="The logo held aloft" width="200" height="150" class="alignnone size-thumbnail-200 wp-image-264" /></a>
+			<h2>Welcome to <?php bloginfo( 'title' ); if ( is_home() ) { echo ' blog'; } ?></h2>
 
-</div><!-- /splash -->
+			<p>The Ball 2006, known for the first time as <em>The Spirit of Football</em>, embodied the motto of the 2006 World Cup: <em>A Time to make Friends</em>. Its short journey to Munich brought people together in the spirit of the beautiful game.</p>
 
+			<?php if ( ! is_home() ) { ?>
+				<p id="gotoblog"><a href="/2006/blog/">Read the blog &rarr;</a></p>
+			<?php } ?>
 
+		</div><!-- /banner_copy -->
 
-<div id="banner_copy">
+	</div><!-- /site_banner_inner -->
 
-<h2>Welcome to <?php bloginfo( 'title' ); if ( is_home() ) { echo ' blog'; } ?></h2>
-
-<p>The Ball 2006, known for the first time as <em>The Spirit of Football</em>, embodied the motto of the 2006 World Cup: <em>A Time to make Friends</em>. Its short journey to Munich brought people together in the spirit of the beautiful game.</p>
-
-<?php if ( ! is_home() ) { ?>
-	<p id="gotoblog"><a href="/2006/blog/">Read the blog &rarr;</a></p>
-<?php } ?>
-
-</div><!-- /banner_copy -->
-
-
-
-</div><!-- /site_banner_inner -->
 </div><!-- /site_banner -->
 
 
--- assets/includes/supporters_2006.php
+++ assets/includes/supporters_2006.php
@@ -1,17 +1,28 @@
-<!-- Begin sponsors -->
+<?php /*
+================================================================================
+Supporters 2006 Template
+================================================================================
+AUTHOR: Christian Wach <needle@haystack.co.uk>
+--------------------------------------------------------------------------------
+NOTES
+
+--------------------------------------------------------------------------------
+*/
+
+?><!-- assets/includes/supporters_2006.php -->
 <div id="sponsors" class="clearfix">
 
-<div id="sponsor_logos">
+	<div id="sponsor_logos">
 
-<ul>
+		<ul>
 
-<li><a href="http://www.filmsat59.com/" title="Films at 59"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/filmsat59.jpg" alt="Films at 59 Logo" /></a></li>
+			<li><a href="http://www.filmsat59.com/" title="Films at 59"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/filmsat59.jpg" alt="Films at 59 Logo" /></a></li>
 
-<li><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/juniperblue.png" alt="JuniperBlue Logo" /></li>
+			<li><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/juniperblue.png" alt="JuniperBlue Logo" /></li>
 
-</ul>
+		</ul>
 
-</div><!-- sponsor_logos -->
+	</div><!-- sponsor_logos -->
 
 </div><!-- /sponsors -->
 
